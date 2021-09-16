48312561a7568cec6623b01d3a613a4a8b281f11
Sam <rieszein@gmail.com>
2021-09-15 21:29:48 -0500

    Inicio de Pokemon Dictionary

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PokemonDictionary());
+}
+
+class PokemonDictionary extends StatefulWidget {
+  PokemonDictionary({Key? key}) : super(key: key);
+
+  @override
+  _PokemonDictionaryState createState() => _PokemonDictionaryState();
+}
+
+class _PokemonDictionaryState extends State<PokemonDictionary> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
